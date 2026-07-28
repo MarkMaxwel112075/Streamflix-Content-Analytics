f8f90b5828749e6d7e0cff034fe393ec276dc5de
Mark_Maxwel_Louis <markmaxwellouis@gmail.com>
2026-07-28 10:11:23 +0530

    Add files via upload

--- sql/Phase3_SQL_Queries.sql
+++ sql/Phase3_SQL_Queries.sql
@@ -1,1 +1,93 @@
+%PDF-1.4
+%“Œ‹ž ReportLab Generated PDF document (opensource)
+1 0 obj
+<<
+/F1 2 0 R /F2 3 0 R
+>>
+endobj
+2 0 obj
+<<
+/BaseFont /Helvetica /Encoding /WinAnsiEncoding /Name /F1 /Subtype /Type1 /Type /Font
+>>
+endobj
+3 0 obj
+<<
+/BaseFont /Courier /Encoding /WinAnsiEncoding /Name /F2 /Subtype /Type1 /Type /Font
+>>
+endobj
+4 0 obj
+<<
+/Contents 9 0 R /MediaBox [ 0 0 595.2756 841.8898 ] /Parent 8 0 R /Resources <<
+/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
+>> /Rotate 0 /Trans <<
 
+>> 
+  /Type /Page
+>>
+endobj
+5 0 obj
+<<
+/Contents 10 0 R /MediaBox [ 0 0 595.2756 841.8898 ] /Parent 8 0 R /Resources <<
+/Font 1 0 R /ProcSet [ /PDF /Text /ImageB /ImageC /ImageI ]
+>> /Rotate 0 /Trans <<
+
+>> 
+  /Type /Page
+>>
+endobj
+6 0 obj
+<<
+/PageMode /UseNone /Pages 8 0 R /Type /Catalog
+>>
+endobj
+7 0 obj
+<<
+/Author (\(anonymous\)) /CreationDate (D:20260724130056+00'00') /Creator (\(unspecified\)) /Keywords () /ModDate (D:20260724130056+00'00') /Producer (ReportLab PDF Library - \(opensource\)) 
+  /Subject (\(unspecified\)) /Title (\(anonymous\)) /Trapped /False
+>>
+endobj
+8 0 obj
+<<
+/Count 2 /Kids [ 4 0 R 5 0 R ] /Type /Pages
+>>
+endobj
+9 0 obj
+<<
+/Filter [ /ASCII85Decode /FlateDecode ] /Length 1094
+>>
+stream
+Gb!SkCNIrc&H/2-EPC3OAVu?!Op2aePgkC@H*oPA(rCM37nF,<<K`jJhm8Iba!&-8Gn2j%,dt+-G'/hPg=U)dgG[qT$p=:RnB?W8!,iAEa?"q)LZ:7p)YU;pE<i&CFjPD]ga8*%5@_OmWJHs?Ho>Sg2-S!r//j2LFujH7h0s1JiPgJ"J<#BQ=<3CXI..-^Ic.U=mfU6"#00_qZhQ'YDpV2l`=mrhKLLS%",X9R5O^qgmDhjH3=\?HkC&l`>_HA5%>HFr:[i8;`((6gc&,$TVg0]Jf8T"ghNANp'-(c/22tZ`-KZM3c*OLqigtf3Q98bC94koNMCeU3*_+qbj7ONsUAZq6Jq]%\?olt;EQjeZ;a#[QVKj#X9opLW/E&C,h+NC225a#EEaDN>E9\+&D`EoRoQTG3KisO35mh+iXsd,2Z5E]/+?DV5qb;s3]rg@FL8o$jF5);5Am"Oj&=q8O.TJ%:MQ'uT\`I9V8(Zue>n>*#6l7^Wpr7$_)FsV7(qgu^:7CV:_5+WI%$3]s"]Jiupi2Zpp@&X\QIX]E:"Lb1)9b%UG0mgRk"V<=9?Skq`?q:bMpYr"*(lKR!ip)*eYkE[$5KNCKb7Tpk0=KMG_T4@;`VL``5d-%p@ASk#,@)Tk<^-;o#7TgG+[>gXdD7"RC0!qUj8]YE[?0MbjNr-E`l]nj.]4kQg`A;pcV<Cp0=d[1i_+]Q+eQ2hk(&MHmqIKUWLe/3rEhUI[Sp!oR:*/b+dc4oV`,+mM>fKJs.b<K#bH!P2q3nh4T'lE$hMWI&?XKDccI5%ps\]G+noaCAIa<YB.@WrV$$B66B>IUt9p"?V>X1E2f/H-]guL:o3d<NNGj!ON(GA2)i-NX!7EC7J,I]Sm9"V&N:$n=eKLbZ0R+Y4o"?.dL:PB'n0]Pc)0Lch&*D-Oji`oSZ'^-9XXjE=7WMR\2O=NSoX/-rNsGn[\u>o0+DZ'W0\^B[h$l)CEgK@2=;S)Kt8Y7K/YHS+Y#22[QON(L=<':(*!Zp5UuqDE[_=hCO2D7WA=J5mE$t0?e?I@\qX)VbKYl$K?U5K&E/jK-cm2$"J[l/<2;BSeLp^YEo'(A'/`/*#Q~>endstream
+endobj
+10 0 obj
+<<
+/Filter [ /ASCII85Decode /FlateDecode ] /Length 1001
+>>
+stream
+Gat=*D/\,^&H9tY\.>&s.iK&!3kD;S>0+-IjeX-!RY54^_*Bp<0\ZWums!%ri?ch\"=1_)F6D_s4(EhuD_(h\!e:LllJF.s)W4B,\i-_-I0/0*r2j)#kGK(PRgnDTFkab\.N\nI9g]LhV:]<D"g+/sUR5$m.sn4?"4rO0JZ&V/2k[#+>=M8\=,6L5h0)(&b6?uqD3h8H!UalF0;/`pX5+ljgi9an3-66hZ*0*mV'l1B-AXl(?blUO[S-/k^d3Lr\2kUPa47T&VTPBl8L18*6V[]T,=3Dg9Z+f@Zb^Ba,n]BV7cn,),[$jUl]k!?k4H9I7%='OKjk/Mh"\5UfIJ9X')K<?BQ+4Bh3Ip6n$o=t4T$ZaQQ,fsa*+hp/4\pV_tKG@5PnE`Y2Hj%5_lIhO$P&UN.IKtYshFL=rW[XVbF/7<"pZ*Ea5W#ASTXHMbhl3S-:@C*`?,P@d,<S:.Z5fD>`SpAQq/?0*==&eu\u?GpG@W0eo>K!n:0JMT,>eV!W2t6:D^O<8#:ag:eZ_nFFKbJM0S%mcn!almFWX?mCk,6,qJs>e^ZS?:]BHpp!'2>9q;g3s53Qmf*F_1sL<i?&]4;*gau(%HrOObnh=<Qkp(fo,1l:(loLL>h<sLMO*_f)2r<uLrK&rq-pZ`BQ:0X#s;>tX<eM$GZ$A]g]J_lb*cj5Bmo:i3tcLn\MQ>^b=LA\3Qhl1O(0!E\HCG$fd2kd#/'0V>$u,s(\/:b@CIMQ0Wn$Z@M?9JRPZ/)lqY%-3l</#/)dh@m?$=n2+*N=*_YrQcVq'g$T$[d8Tj/W.j;3L1Bb,(D1dmG;?co[9rG_g1[]*D-O::0EPORE+0:oFRLi5&J_G/beQsY(%:1tIA,L:L.g(\TTOL8to?":97Z8c6d?o2b+^7K=%Erpu0,^AnKdfU[)L8^$G>XQDQVM&('2>0'cotT\5>#s*S5!:ECF8#K#GUnSYVDBG(/0<@4=Tu;4?P=kJThV7r$1l>J]^3R0>RM~>endstream
+endobj
+xref
+0 11
+0000000000 65535 f 
+0000000061 00000 n 
+0000000102 00000 n 
+0000000209 00000 n 
+0000000314 00000 n 
+0000000517 00000 n 
+0000000721 00000 n 
+0000000789 00000 n 
+0000001069 00000 n 
+0000001134 00000 n 
+0000002319 00000 n 
+trailer
+<<
+/ID 
+[<0a2ef2bd7f8c48ad8e60de69f35c7110><0a2ef2bd7f8c48ad8e60de69f35c7110>]
+% ReportLab generated PDF document -- digest (opensource)
+
+/Info 7 0 R
+/Root 6 0 R
+/Size 11
+>>
+startxref
+3412
+%%EOF
